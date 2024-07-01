36c9d657b587f0b876f3a81d3855b2c6767e27d1
Krystian Karczmarzyk <kwantowebananyhiman@gmail.com>
2024-07-01 22:13:40 +0200

    initial commit

--- langauge-journal/src/app/sing-in-up/sing-in-up.component.ts
+++ langauge-journal/src/app/sing-in-up/sing-in-up.component.ts
@@ -1,20 +1,58 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup, NgForm, ReactiveFormsModule, Validators,
+  NgModel
+} from "@angular/forms";
+
+
+
+function createCompareValidator(controlOne: AbstractControl, controlTwo: AbstractControl) {
+  return () => {
+    if (controlOne.value !== controlTwo.value)
+      return { match_error: 'Value does not match' };
+    return null;
+  };
+
+}
+
 
 @Component({
   selector: 'app-sing-in-up',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './sing-in-up.component.html',
   styleUrl: './sing-in-up.component.scss'
 })
-export class SingInUpComponent implements AfterViewInit{
+export class SingInUpComponent implements AfterViewInit, OnInit{
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  registrationForm: FormGroup;
+
+  username = new FormControl('', [Validators.required, Validators.min(5), Validators.max(15)])
+  email = new FormControl('', [Validators.email, Validators.required])
+  password = new FormControl('', [
+    (c: AbstractControl) => Validators.required(c),
+    Validators.pattern(
+      /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+    ),
+  ]);
+  confirmPassword = new FormControl('', [
+    (c: AbstractControl) => Validators.required(c),
+    Validators.pattern(
+      /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+    ),
+  ]);
 
 
-  // @ViewChild('SignUp') signUp! : ElementRef
-  // @ViewChild('SignIn') signIn! : ElementRef
+
   @ViewChild('container',) container! : ElementRef
-
-
 
   signUpClick() {
     this.container.nativeElement.classList.add("right-panel-active");
@@ -31,6 +69,24 @@ export class SingInUpComponent implements AfterViewInit{
   ngAfterViewInit() {
     console.log(this.container.nativeElement.classList)
 
+  }
+
+  ngOnInit() {
+
+    this.registrationForm = this.formBuilder.group({
+      username: this.username,
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword
+    });
+    this.registrationForm.setValidators(createCompareValidator(this.password, this.confirmPassword))
+
+
+  }
+
+
+  onSubmit() {
+    console.log(this.registrationForm)
   }
 }
 
