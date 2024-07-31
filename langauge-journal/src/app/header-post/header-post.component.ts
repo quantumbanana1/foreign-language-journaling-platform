4a3e1db7b9c6f5373ee710a8eb0c723b3d033f70
Krystian Karczmarzyk <kwantowebananyhiman@gmail.com>
2024-08-01 00:16:57 +0200

    Add drop-down-mechanism and mechanism for adding and deleting badge

--- langauge-journal/src/app/header-post/header-post.component.ts
+++ langauge-journal/src/app/header-post/header-post.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {LanguageIndicatorComponent} from "../language-indicator/language-indicator.component";
 import {InterestsIndicatorComponent} from "../interests-indicator/interests-indicator.component";
+import {InputPostBindingService} from "../input-post-binding.service";
 
 @Component({
   selector: 'app-header-post',
@@ -12,6 +13,22 @@ import {InterestsIndicatorComponent} from "../interests-indicator/interests-indi
   templateUrl: './header-post.component.html',
   styleUrl: './header-post.component.scss'
 })
-export class HeaderPostComponent {
+export class HeaderPostComponent implements OnInit {
+
+  public title: string = ''
+  @ViewChild('titleElement') titleElement: ElementRef;
+
+  constructor(private inputBindingsService: InputPostBindingService) { }
+
+  ngOnInit() {
+
+    this.inputBindingsService.inputData.subscribe((data:string) => {
+      this.title = data;
+      if (this.title === '') {
+
+      }
+
+    })
+  }
 
 }
--- langauge-journal/src/app/selection-block-post/selection-block-post.component.ts
+++ langauge-journal/src/app/selection-block-post/selection-block-post.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {CommonModule} from "@angular/common";
+import {InputPostBindingService} from "../input-post-binding.service";
+import {FormsModule} from "@angular/forms";
+import {BadgeButtonFromSelectionComponent} from "../badge-button-from-selection/badge-button-from-selection.component";
+import {SelectInputComponent} from "../select-input/select-input.component";
 
 @Component({
   selector: 'app-selection-block-post',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, BadgeButtonFromSelectionComponent, SelectInputComponent],
   templateUrl: './selection-block-post.component.html',
   styleUrl: './selection-block-post.component.scss'
 })
-export class SelectionBlockPostComponent {
+export class SelectionBlockPostComponent  {
+
+  public selectedInterests: string[] = []
+  public titleValue: string = '';
+
+  constructor(private inputBindingsService: InputPostBindingService) { }
+
+
+  onChangeInput(eventTarget: EventTarget) {
+    const val = (eventTarget as HTMLInputElement).value;
+    this.inputBindingsService.updateValue(val);
+
+  }
+
 
 }
